b9c01adf95f1ef0b23e3553afd6e8e778130b2c0
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2022-04-08 09:04:02 +0530

    Update lab2_q1.cpp

--- Lab2/lab2_q1.cpp
+++ Lab2/lab2_q1.cpp
@@ -3,22 +3,16 @@ using namespace std;
 const int N = 100;
 vector<int> graph[N];
 vector <int> vis;
-int cnt;
 
 void dfs(int vertex)
 {
-    cnt++;
     cout<<vertex<<" ";
-    cnt++;
     vis.push_back(1);
     for(int child : graph[vertex])
     {
-        cnt++;
         cout<<"parent : "<<vertex<<"child : "<<child<<endl;
-        cnt++;
         if(vis[child])
         {
-            cnt++;
             continue;
         }
         dfs(child);
@@ -29,31 +23,18 @@ void dfs(int vertex)
 
 int main()
 {
-    cnt++;
     int n,m;
-    cnt++;
     cin>>n>>m;
-    cnt++;
     for(int i=0;i<m;i++)
     {
-        cnt++;
         int v1,v2;
-        cnt++;
         cin>>v1>>v2;
-        cnt++;
         graph[v1].push_back(v2);
-        cnt++;
         graph[v2].push_back(v1);
     }
-    cnt++;
-    cnt++;
     int vert;
-    cnt++;
     cout<<"Enter vertex to start with : "<<endl;
-    cnt++;
     cin>>vert;
-    cnt++;
-    cout<<"Total number of steps : "<<cnt<<endl;
     
 
 }
